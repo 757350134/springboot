288ac53ff71a6733f78f2ad776d800bd7b738616
wxp <757350134@qq.com>
2017-01-19 14:59:21 +0800

    springboot

---
+++ src/main/java/com/cntv/propertiestest/DefaultProperties.java
@@ -0,0 +1,66 @@
+package com.cntv.propertiestest;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix="server.info")
+public class DefaultProperties {
+
+	private String address;
+	private String username;
+	private String password;
+	
+	
+	
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "DefaultProperties [userName=" + username + ", password="
+				+ password + ", address=" + address + "]";
+	}
+	
+	
+}
